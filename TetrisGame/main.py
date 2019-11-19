ba4796ca66a3f5be7680243523bc361a91467959
BruceMT <wma7@stevens.edu>
2019-11-19 00:10:25 -0500

    main area

--- TetrisGame/main.py
+++ TetrisGame/main.py
@@ -1,8 +1,11 @@
 # @Author  : weibin Ma
 # @Time    : 10/30/2019
 # @title: pythonPJ
+# test push
 import sys
 import pygame
+from settings import *
+
 
 def main():
 
@@ -24,8 +27,44 @@ def main():
  #Fill in the screen background color
         screen.fill(bg_color)
 
-          #redraw
+        #draw the line
+        draw_game_area(screen)
+
+        #draw block
+        draw_cell(screen, GAME_AREA_LEFT, GAME_AREA_TOP)
+        # make draw visible
         pygame.display.flip()
+
+# dtaw the game area
+def draw_game_area(screen):
+        #color(rgb), start node, end node
+
+        pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LEFT, GAME_AREA_TOP),
+                         (GAME_AREA_LEFT + GAME_AREA_WIDTH, GAME_AREA_TOP))
+        # bottom
+        for i in range(21):
+            pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LEFT, GAME_AREA_TOP + i* CELL_WIDTH),
+                         (GAME_AREA_LEFT + GAME_AREA_WIDTH, GAME_AREA_TOP + i* CELL_WIDTH))
+        # left
+        pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LEFT, GAME_AREA_TOP),
+                         (GAME_AREA_LEFT, GAME_AREA_TOP + 20 * CELL_WIDTH))
+        # right
+        for i in range(11):
+           pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LEFT + i * CELL_WIDTH, GAME_AREA_TOP),
+                         (GAME_AREA_LEFT + i * CELL_WIDTH, GAME_AREA_TOP + 20 * CELL_WIDTH))
+
+def draw_cell(screen,left,top):
+    '''
+        left: the length from the window left
+        top: the length from the window top
+    '''
+    cell_left_top = (left+ 4*CELL_WIDTH, top)  # block left top
+
+    cell_width_height = (CELL_WIDTH, CELL_WIDTH)  # width and height of bolck
+
+    cell_rect = pygame.Rect(cell_left_top , cell_width_height)  # draw the left-top rect
+
+    pygame.draw.rect(screen, CELL_COLOR, cell_rect)  # draw rect
 
 if __name__ == '__main__':
        main()
---
+++ TetrisGame/settings.py
@@ -0,0 +1,14 @@
+'''
+some constants
+'''
+
+SCREEN_WIDTH = 1200    #width
+SCREEN_HEIGHT = 900    #height
+CELL_WIDTH = 40         #game area 20x10 every length is 40 pixles .
+GAME_AREA_WIDTH = CELL_WIDTH * 10       #10 colums
+GAME_AREA_HEIGHT = CELL_WIDTH * 20      #20 rowa
+GAME_AREA_LEFT = (SCREEN_WIDTH - GAME_AREA_WIDTH) // 2      #left blank
+GAME_AREA_TOP = SCREEN_HEIGHT - GAME_AREA_HEIGHT          #top blank
+EDGE_COLOR = (0, 0, 0)          #edge color
+CELL_COLOR = (100, 100, 100)    #block color
+BG_COLOR = (230, 230, 230)      #background color
